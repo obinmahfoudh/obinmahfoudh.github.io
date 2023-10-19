5064c9d1fb9aa86d32d820ec76a929078764b15d
obinmahfoudh <binmahfoudhobaid@gmail.com>
2023-10-19 01:04:54 -0500

    Added color change to hovered class, added fix for directly accessing root-relative links

--- src/Pages/Classes.js
+++ src/Pages/Classes.js
@@ -3,18 +3,23 @@ import {Courses} from "../ClassList"
 export const Classes = () => {
   return (
     <>
-      <div class = "text-white p-40 gap-40 flex flex-col ">
-        <div class= 'flex justify-between'>
+    {document.title = 'Classes'}
+      <div class = "text-white p-40 gap-40 flex flex-col justify-center">
+        <div class= 'flex '>
           <h1 class='text-4xl  font-extrabold'>Computer Science and Engineering</h1>
           {getCourses("Computer Science and Engineering")}
         </div>
         <div class= 'flex justify-between'>
           <h1 class='text-4xl flex j font-extrabold'>Math</h1>
-          {getCourses("Math")}
+          <div class='place-self-center px-80'>
+            {getCourses("Math")}
+          </div>
         </div>
         <div class= 'flex justify-between'>
           <h1 class='text-4xl font-extrabold'>Sciences</h1>
-          {getCourses("Sciences")}
+          <div class='place-self-center px-80' >
+            {getCourses("Sciences")}
+          </div>
         </div>
       </div>
     </>
@@ -26,13 +31,10 @@ function getCourses(courseType){
   const computerCourses= Courses.filter(Courses => Courses.Type === courseType);
   const listItems = computerCourses.map(Courses =>
     <li>
-      <p class = 'transition ease-in-out delay-150  hover:scale-125 duration-700'>
-        <b class= 'text-white bg-sky-500 rounded-md h-10 w-40'>{Courses.Name}</b>
+      <p class = 'transition ease-in-out delay-150  hover:scale-125  duration-700'>
+        <b class= 'text-white bg-sky-500 rounded-md p-1 hover:bg-red-300 hover:underline'>   {Courses.Name} </b>
       </p>
     </li>
     )
-  return <ul class='flex flex-row flex-wrap justify-center space-x-4 '>{listItems}</ul>
+  return <ul class='flex flex-wrap justify-center gap-6'>{listItems}</ul>
 }
-
-
-//{Object.keys(Courses)}
